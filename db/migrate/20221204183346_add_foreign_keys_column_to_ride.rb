5d35825b84f060309681834e69f3682a0c20a649
Ashley <ashleynjeri7@gmail.com>
2022-12-04 21:42:01 +0300

    Associations Added to Models

--- db/migrate/20221204183346_add_foreign_keys_column_to_ride.rb
+++ db/migrate/20221204183346_add_foreign_keys_column_to_ride.rb
@@ -1,5 +1,6 @@
 class AddForeignKeysColumnToRide < ActiveRecord::Migration[6.1]
   def change
+    # rails g migration AddForeignKeysColumnToRide passenger_id:integer taxi_id:integer
     add_column :rides, :passenger_id, :integer
     add_column :rides, :taxi_id, :integer
   end
